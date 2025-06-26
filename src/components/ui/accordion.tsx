a2d7fb2b8ef7783484060a70470f280d425542c6
Aashis719 <aashishnep10@gmail.com>
2025-06-26 21:13:57 +0545

    ui component

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import * as AccordionPrimitive from "@radix-ui/react-accordion"
 import { ChevronDown } from "lucide-react"
-
+//// This file is part of the Locus Tours and Travels project.
 import { cn } from "@/lib/utils"
 
 const Accordion = AccordionPrimitive.Root
