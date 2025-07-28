c26f9fcad1f10cd177645a9cec11e846c2e0af48
Aashis719 <aashishnep10@gmail.com>
2025-07-28 22:33:15 +0545

    Services Section updated

--- src/components/ServicesSection.tsx
+++ src/components/ServicesSection.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import AnimatedText from './ui/AnimatedText';
 
 const services = [
-  {
+  { 
     icon: (
       <svg width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
         <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
